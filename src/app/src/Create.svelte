308159c235e706444fdf52724ea95cbe5b4e4b01
Sueqkjs <sueqk@outlook.jp>
2022-02-21 21:19:15 +0900

    no password

--- src/app/src/Create.svelte
+++ src/app/src/Create.svelte
@@ -6,6 +6,7 @@
     Block,
     BlockHeader,
     BlockTitle,
+    Checkbox,
     ListInput,
     Button,
     f7,
@@ -18,6 +19,7 @@
   $: title = "";
   $: content = "";
   $: isLoading = false;
+  let noPassword = false;
   let root;
   let _;
 
@@ -27,32 +29,33 @@
   });
 
   async function post() {
-    let showPassword = "";
-    f7.dialog.password(
-      "This Pasta's Password",
-      "PastaBin",
-      async (v: string) => {
-        showPassword = v;
-        if (showPassword.trim().length === 0)
-          f7.dialog.alert("Warning: empty password");
-        isLoading = true;
-        let result = _("#result");
-        let input = _("#input");
-        let key = rand(32);
-        let nonce = rand(12);
-        content = content
-          .replaceAll(/<div( class="")?>/g, "\n")
-          .replaceAll("</div>", "")
-          .replaceAll("&nbsp;", " ");
-        let { id } = await (
-          await fetch("api/pasta", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              title,
-              showPasswordHash: aes.toHex(
+    let showPassword = noPassword
+      ? ""
+      : await passwordAsync("This Pasta's password", "PastaBin").catch(
+          () => ""
+        );
+    if (showPassword.trim().length === 0)
+      f7.dialog.alert("Warning: empty password");
+    isLoading = true;
+    let result = _("#result");
+    let input = _("#input");
+    let key = rand(32);
+    let nonce = rand(12);
+    content = content
+      .replaceAll(/<div( class="")?>/g, "\n")
+      .replaceAll("</div>", "")
+      .replaceAll("&nbsp;", " ");
+    let { id } = await (
+      await fetch("api/pasta", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          title,
+          showPasswordHash: !showPassword
+            ? ""
+            : aes.toHex(
                 new Uint8Array(
                   await crypto.subtle.digest(
                     "SHA-512",
@@ -60,28 +63,28 @@
                   )
                 )
               ),
-              content: aes.toHex(
-                aes.encrypt(key, nonce, aes.toU8(content, true))
-              ),
-            }),
-          })
-        )
-          .json()
-          .catch((e) => {
-            isLoading = false;
-          });
-        resultId = id;
-        resultKey = aes.toHex(key);
-        resultNonce = aes.toHex(nonce);
-        result.style.display = "block";
-        input.style.display = "none";
-      },
-      () => void 0
-    );
+          content: aes.toHex(aes.encrypt(key, nonce, aes.toU8(content, true))),
+        }),
+      })
+    )
+      .json()
+      .catch(() => {
+        isLoading = false;
+      });
+    resultId = id;
+    resultKey = aes.toHex(key);
+    resultNonce = aes.toHex(nonce);
+    result.style.display = "block";
+    input.style.display = "none";
   }
 
   function rand(bit: number): Uint8Array {
     return crypto.getRandomValues(new Uint8Array(bit));
+  }
+  function passwordAsync(title: string, text: string): Promise<string> {
+    return new Promise((res, rej) => {
+      f7.dialog.password(title, text, res, rej);
+    });
   }
 </script>
 
@@ -90,6 +93,12 @@
     <div id="input">
       <BlockTitle>Boil the pasta</BlockTitle>
       <BlockHeader>Warning: Title value was not encrypted</BlockHeader>
+      <BlockHeader
+        >No Password (not recommented): <Checkbox
+          value={noPassword}
+          on:change={() => (noPassword = !noPassword)}
+        /></BlockHeader
+      >
       <List>
         <ListInput
           type="texteditor"
@@ -135,14 +144,16 @@
             navigator.clipboard
               .writeText(_("#url").innerHTML.replaceAll("&amp;", "&"))
               .then(() => {
-                f7.notification.create({
-                  icon: "<i class=\"f7-icons\" style=\"font-size: 16px; width: 16px; height: 16px;\">house_fill</i>",
-                  title: "PastaBin",
-                  titleRightText: "now",
-                  subtitle: "Copied!",
-                  text: "Been copied the url to your clipboard",
-                  closeTimeout: 1500,
-                }).open();
+                f7.notification
+                  .create({
+                    icon: '<i class="f7-icons" style="font-size: 16px; width: 16px; height: 16px;">house_fill</i>',
+                    title: "PastaBin",
+                    titleRightText: "now",
+                    subtitle: "Copied!",
+                    text: "Been copied the url to your clipboard",
+                    closeTimeout: 1500,
+                  })
+                  .open();
               })
               .catch(alert);
           }}
